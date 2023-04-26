a8f7978af35e145fa214e75d01403ca5cc34791b
Mustafa <m.badak99@gmail.com>
2023-04-26 11:02:28 +0200

    v2

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -2,6 +2,9 @@ public class AtmApp {
     public static void main(String[] args) {
         System.out.println("Bu bir Atm Projesidir");
 // version 1
+        System.out.println("Java ist easy");
+        //version 2
+
 
 
 
